3dcf39f2e72f9c581f801d997192496ac3b96078
minpaint <MINPAINT@yandex.by>
2025-01-26 12:51:50 +0300

    Delete core directory

--- core/models/base.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-class BaseModel(models.Model):
-    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Создано")
-    updated_at = models.DateTimeField(auto_now=True, verbose_name="Обновлено")
-
-    class Meta:
-        abstract = True
